981c6f40f1ad76cc410149d0062362ee8793bf29
Vladislav <vladislavkoboziev@qmail.com>
2019-06-04 15:00:09 +0300

    Commit

--- csvparsing.go
+++ csvparsing.go
@@ -8,41 +8,44 @@ import (
 	"strconv"
 	"time"
 )
-var people [] Person  ///slice with our data
+
+var people []Person ///slice with our data
 type Gender int
+
 const (
 	Male Gender = iota
 	Female
 	gender
 )
-type Person struct{
-	Id int
-	FirstName string
-	LastName string
-	Email string
-	Gender string
+
+type Person struct {
+	Id               int
+	FirstName        string
+	LastName         string
+	Email            string
+	Gender           string
 	DateRegistration time.Time
-	Loan string
+	Loan             string
 }
+
 func (g Gender) String() string {
 	return [...]string{"Male", "Female", "gender"}[g]
 }
-func parsing_csv()  {
-	csvFille,_ := os.Open("MOCK_DATA.csv")
+func parsing_csv() {
+	csvFille, _ := os.Open("MOCK_DATA.csv")
 	reader := csv.NewReader(csvFille)
-	for i:= 0; i<101; i++{
-		line,_ := reader.Read()
-		id,_ := strconv.Atoi(line[0])
-		dataPeople,_ := time.Parse("1/2/2006",line[5])
-		people = append(people,Person{
-			Id:					id,
-			FirstName:			line[1],
-			LastName:			line[2],
-			Email:				line[3],
-			Gender:				line[4],
-			DateRegistration:	dataPeople,
-			Loan:line[6],
-
+	for i := 0; i < 101; i++ {
+		line, _ := reader.Read()
+		id, _ := strconv.Atoi(line[0])
+		dataPeople, _ := time.Parse("1/2/2006", line[5])
+		people = append(people, Person{
+			Id:               id,
+			FirstName:        line[1],
+			LastName:         line[2],
+			Email:            line[3],
+			Gender:           line[4],
+			DateRegistration: dataPeople,
+			Loan:             line[6],
 		})
 	}
 
@@ -53,16 +56,15 @@ func parsing_csv()  {
 	//prozent_Man_Woman()
 	//genderPercent()
 	//sortLoan()
-	for _, value := range people{
+	for _, value := range people {
 		t := value.DateRegistration
 		m := value.DateRegistration
-		dataPeople(t,m,people)
-}
+		dataPeople(t, m, people)
+	}
 
+}
 func main() {
-
-}
-
+	parsing_csv()
 }
 
 func loanrange() {
@@ -70,31 +72,31 @@ func loanrange() {
 		first_range float64 = 253372.042169
 		last_range  float64 = 571786.599688
 	)
-		for i := 0; i < len(people); i++ {
+	for i := 0; i < len(people); i++ {
 		fmt.Println(people[i].Loan)
+	}
+	fmt.Println("До проверки по диапозону")
+	for i := 0; i < len(people); i++ {
+		currLoan1, _ := strconv.ParseFloat(people[i].Loan, 64)
+		if currLoan1 >= first_range && currLoan1 <= last_range {
+			fmt.Println(people[i])
 		}
-		fmt.Println("До проверки по диапозону")
-			for i:= 0; i< len(people); i++{
-				currLoan1,_ := strconv.ParseFloat(people[i].Loan, 64)
-				if currLoan1>=first_range && currLoan1 <= last_range  {
-					fmt.Println(people[i])
-				}
 	}
 }
-func prozent_Man_Woman()  {
+func prozent_Man_Woman() {
 	var m int
 	var f int
-	for _, value := range people{
-		if value.Gender == ""{
-			}else if value.Gender == "Male"{
+	for _, value := range people {
+		if value.Gender == "" {
+		} else if value.Gender == "Male" {
 			m++
-			} else if value.Gender == "Female"{
-				f++
-			}
+		} else if value.Gender == "Female" {
+			f++
+		}
 
- 	}
-	fmt.Println("Женщины",(f*len(people)-1)/100,"%")
-	fmt.Println("Мужчины",(m*len(people)-1)/100,"%")
+	}
+	fmt.Println("Женщины", (f*len(people)-1)/100, "%")
+	fmt.Println("Мужчины", (m*len(people)-1)/100, "%")
 }
 func genderPercent() {
 	countMale := 0
@@ -124,7 +126,7 @@ func genderPercent() {
 	fmt.Println("Percent of men", percentMale)
 	fmt.Println("Percent of women", percentFemale)
 }
-func dataPeople(dateFirst, dateLast time.Time, people []Person) {   //////// date!!!!!!!!
+func dataPeople(dateFirst, dateLast time.Time, people []Person) { //////// date!!!!!!!!
 
 	for _, value := range people {
 
@@ -137,10 +139,11 @@ func dataPeople(dateFirst, dateLast time.Time, people []Person) {   //////// dat
 	}
 
 }
-func sortLoan() {         /// sort the data by Loan
+func sortLoan() { /// sort the data by Loan
 	sort.Slice(people, func(i, j int) bool {
 		return people[i].Loan < people[j].Loan
 	})
 	for _, value := range people {
 		fmt.Println(value)
-	}}
+	}
+}
